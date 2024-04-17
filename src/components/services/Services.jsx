1c6822a5047c416ea626e7c4ca17f68ba7a16e6d
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-04-18 03:36:29 +0600

    working on services section

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,10 @@
+
+const Services = () => {
+    return (
+        <div className="w-full h-[30vh] bg-[#28282B]">
+            <h1 className="text-2xl text-center pt-20 font-bold">Our Services</h1>
+        </div>
+    );
+};
+
+export default Services;
--- src/layout/Home/Home.jsx
+++ src/layout/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../../components/banner/Banner";
 import AllCollectionTypes from "../../components/collection/AllCollectionTypes";
 import Location from "../../components/location/Location";
 import Promo from "../../components/promoSection/Promo";
+import Services from "../../components/services/Services";
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <AllCollectionTypes></AllCollectionTypes>
+            <Services></Services>
             <Promo></Promo>
             <Location></Location>
         </div>
