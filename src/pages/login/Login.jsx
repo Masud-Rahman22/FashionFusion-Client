8b1febddb2b3bf88bf41daa29d6f05666adc12d4
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-04-19 02:21:04 +0600

    worked on jwt token and fixed some styling issue in login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
             })
     }
     const onSubmit = ({ email, password }) => {
-        // Email Password signup
+        // Email Password signUp
         dispatch(loginUser({
             email,
             password,
@@ -72,7 +72,7 @@ const Login = () => {
             <div onClick={() => setOpenModal(false)} className={`fixed z-[100] flex items-center justify-center ${openModal ? 'opacity-1 visible' : 'invisible opacity-0'} inset-0 h-full w-full bg-black/20 backdrop-blur-sm duration-100`}>
                 <Toaster />
                 <div onClick={(e_) => e_.stopPropagation()} className={`absolute w-full rounded-lg bg-white dark:bg-gray-900 drop-shadow-2xl sm:w-[500px] ${openModal ? 'opacity-1 translate-y-0 duration-300' : '-translate-y-20 opacity-0 duration-150'}`}>
-                    <form onSubmit={handleSubmit(onSubmit)} className="px-5 pb-5 pt-3 lg:pb-10 lg:pt-5 lg:px-10">
+                    <form onSubmit={handleSubmit(onSubmit)} className="px-5 pb-5 pt-3 lg:pb-10 lg:pt-5 lg:px-10 block">
                         <svg onClick={() => setOpenModal(false)} className="mx-auto mr-0 w-10 cursor-pointer fill-black dark:fill-white" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M6.99486 7.00636C6.60433 7.39689 6.60433 8.03005 6.99486 8.42058L10.58 12.0057L6.99486 15.5909C6.60433 15.9814 6.60433 16.6146 6.99486 17.0051C7.38538 17.3956 8.01855 17.3956 8.40907 17.0051L11.9942 13.4199L15.5794 17.0051C15.9699 17.3956 16.6031 17.3956 16.9936 17.0051C17.3841 16.6146 17.3841 15.9814 16.9936 15.5909L13.4084 12.0057L16.9936 8.42059C17.3841 8.03007 17.3841 7.3969 16.9936 7.00638C16.603 6.61585 15.9699 6.61585 15.5794 7.00638L11.9942 10.5915L8.40907 7.00636C8.01855 6.61584 7.38538 6.61584 6.99486 7.00636Z"></path></g></svg>
                         <h1 className="pb-8 text-4xl backdrop-blur-sm">Login</h1>
                         <div className="space-y-5">
@@ -97,8 +97,6 @@ const Login = () => {
                         </div>
                         {/* here link is to home so further it can be a big problem just for a reminder to change it in future */}
                         <p className='mt-5'>Hey there pal !! you new here ? then hurry up <Link to='/'><button onClick={() => setOpenRegisterModal(true)} className='text-blue-300'>Register</button></Link> now</p>
-
-                        {/* button type will be submit for handling form submission*/}
                         <button className="text-xl w-32 h-14 bg-gray-700 text-white relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-md mt-5"><span className="absolute bg-black size-36 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span><span className="absolute bg-black size-36 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>Login</button>
                         <button onClick={handleGoogleLogin} className="text-xl w-36 ml-10 h-14 bg-gray-700 text-white relative overflow-hidden group z-10 hover:text-white duration-1000 rounded-md mt-5"><span className="absolute bg-black size-40 rounded-full group-hover:scale-100 scale-0 -z-10 -left-2 -top-10 group-hover:duration-500 duration-700 origin-center transform transition-all"></span><span className="absolute bg-black size-40 -left-2 -top-10 rounded-full group-hover:scale-100 scale-0 -z-10 group-hover:duration-700 duration-500 origin-center transform transition-all"></span>Google sign in</button>
                     </form>
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,24 +3,34 @@ import { createContext, useEffect, useState } from "react";
 import { setUser, toggleLoading } from "../redux/features/user/userSlice";
 import { useDispatch } from "react-redux";
 import auth from "../utils/firebase.config";
+import useAxios from "../hooks/useAxios";
 export const AuthContext = createContext(null);
 
 // eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
     const [user,setUsers] = useState({});
     const dispatch = useDispatch()
+    const axios = useAxios()
 
     useEffect(() => {
         onAuthStateChanged(auth, (user) => {
             console.log(user)
             setUsers(user)
             if (user) {
+                const userInfo = {email: user.email}
+                axios.post('/api/jwt', userInfo)
+                .then(res=>{
+                    if(res.data.token){
+                        localStorage.setItem('access-token', res.data.token)
+                    }
+                })
                 dispatch(setUser({
                     name: user.displayName,
                     email: user.email
                 }))
                 dispatch(toggleLoading(false))
             } else {
+                localStorage.removeItem('access-token')
                 dispatch(toggleLoading(false))
             }
         })
