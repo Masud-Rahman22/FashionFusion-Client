dfcd4444890a56539f6bf09250907b43053597ba
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-04-18 23:20:37 +0600

    service section is completed but make sure to add image afterwards

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -7,8 +7,8 @@ const Services = () => {
     return (
         <div className="w-full h-[80vh]">
             <div className="w-full h-[50vh] bg-[#28282B] relative">
-                <h1 className="text-2xl text-center pt-20 font-bold">Our Services</h1>
-                <p>Which we provide</p>
+                <h1 className="text-4xl text-white text-center pt-20 font-bold">Our Services</h1>
+                <p className='text-center text-white font-normal text-xl mt-4'>Which we provide</p>
                 <div className="grid grid-cols-3 gap-20 md:ml-52 absolute mt-20">
                     <Card sx={{ minHeight: '350px', width: 320 }}>
                         <CardCover>
@@ -21,13 +21,18 @@ const Services = () => {
                         </CardCover>
                         <CardCover
                             sx={{
+                                backdropFilter: 'blur(2px)',
+                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
                                 background:
                                     'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
                             }}
                         />
-                        <CardContent sx={{ justifyContent: 'flex-end' }}>
+                        <CardContent sx={{
+                            justifyContent: 'flex-end',
+
+                        }}>
                             <Typography level="title-lg" textColor="#fff">
-                            Curated Selection
+                                Curated Selection
                             </Typography>
                             <Typography
                                 startDecorator={<LocationOnRoundedIcon />}
@@ -48,19 +53,21 @@ const Services = () => {
                         </CardCover>
                         <CardCover
                             sx={{
+                                backdropFilter: 'blur(2px)',
+                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
                                 background:
                                     'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
                             }}
                         />
                         <CardContent sx={{ justifyContent: 'flex-end' }}>
                             <Typography level="title-lg" textColor="#fff">
-                            Personalized Styling
+                                Personalized Styling
                             </Typography>
                             <Typography
                                 startDecorator={<LocationOnRoundedIcon />}
                                 textColor="neutral.300"
                             >
-                                Providing personalized styling services where customers can receive recommendations based on their preferences, body type, and occasion, enhancing their shopping experience and ensuring satisfaction.
+                                Providing personalized styling services where customers can receive recommendations based on their preferences, body type, and occasion, enhancing their shopping experience.
                             </Typography>
                         </CardContent>
                     </Card>
@@ -75,19 +82,21 @@ const Services = () => {
                         </CardCover>
                         <CardCover
                             sx={{
+                                backdropFilter: 'blur(2px)',
+                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
                                 background:
                                     'linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)',
                             }}
                         />
                         <CardContent sx={{ justifyContent: 'flex-end' }}>
                             <Typography level="title-lg" textColor="#fff">
-                            Convenience and Accessibility
+                                Convenience and Accessibility
                             </Typography>
                             <Typography
                                 startDecorator={<LocationOnRoundedIcon />}
                                 textColor="neutral.300"
                             >
-                                Ensuring convenience and accessibility through features like easy navigation, fast shipping, hassle-free returns, and responsive customer support, making shopping for clothing from different brands a seamless and enjoyable process.
+                                Ensuring convenience and accessibility through features like easy navigation, fast shipping, hassle-free returns, and responsive customer support, making shopping for clothing.
                             </Typography>
                         </CardContent>
                     </Card>
