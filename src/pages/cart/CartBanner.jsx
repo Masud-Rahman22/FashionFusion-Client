67e73cbfc5d42111fe1d6e29f4d7f5c853a66818
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-04-28 02:50:50 +0600

    cart's banner added

--- src/pages/cart/CartBanner.jsx
+++ src/pages/cart/CartBanner.jsx
@@ -1,9 +1,9 @@
-
+import cartImage from '../../assets/cart/pexels-karolina-grabowska-5632371.jpg'
 
 const CartBanner = () => {
     return (
         <div>
-            
+            <img className='h-[50vh] w-full' src={cartImage} alt="male models group photo" />
         </div>
     );
 };
