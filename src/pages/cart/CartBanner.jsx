58492a09d78dc9bedbab2611aabb529dbdfe93d9
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-04-29 23:39:04 +0600

    cart section layout with a table of product information and other side the payment information

--- src/pages/cart/CartBanner.jsx
+++ src/pages/cart/CartBanner.jsx
@@ -4,6 +4,16 @@ const CartBanner = () => {
     return (
         <div>
             <img className='h-[50vh] w-full' src={cartImage} alt="male models group photo" />
+            <div className='flex items-center justify-between'>
+                <div>
+                    <h1>here the table will be showed</h1>
+                </div>
+                <div>
+                    <div>
+                        <h1>here the payment section</h1>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
